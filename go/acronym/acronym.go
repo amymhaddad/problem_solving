5ba9fef72e1870f8ff2f367efbb316bce620d6fc
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-28 11:35:45 -0400

    Solved it

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,28 +1,21 @@
-//package main
 package acronym
 
 import (
-	//"fmt"
 	"regexp"
 	"strings"
 )
 
 // Return the acronym of a given phrase
 func Abbreviate(s string) string {
-	r := regexp.MustCompile("[A-Za-z]+'?[A-Za-z]*")
-	words := r.FindAllString(s, -1)
+	match := regexp.MustCompile("[A-Za-z]+'?[A-Za-z]*")
+	words := match.FindAllString(s, -1)
 
 	var acronym = []string{}
-	for _, word := range words{
-		letter := string(word[0])
-		capitalLetter := strings.Title(strings.ToLower(letter))
-		acronym = append(acronym,capitalLetter)
+
+	for _, word := range words {
+		firstLetter := string(word[0])
+		capitalLetter := strings.Title(strings.ToLower(firstLetter))
+		acronym = append(acronym, capitalLetter)
 	}
 	return strings.Join(acronym, "")
- 
 }
-
-// func main() {
-// 	fmt.Println(Abbreviate("There's Hello world-Here"))
-// }
-//
