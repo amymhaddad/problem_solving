675ef1e6ff8223991f87a21167054c0074ff4f8b
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-06 11:07:11 -0400

    twofer round 2

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,10 +1,12 @@
+//twofer problem
 package twofer
+
+import "fmt"
 
 //Given a name, return a string with the message
 func ShareWith(name string) string {
-	if len(name) > 0 {
-		return "One for " + name + ", one for me."
-	} else {
+	if len(name) == 0 {
 		return "One for you, one for me."
 	}
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
