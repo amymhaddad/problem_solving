535326da18b9a22bd474fce527239dc32b5c41ce
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-15 10:45:43 -0400

    First implementation of algo

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	//"strconv"
+	"strconv"
+	"strings"
 )
 
 //Raindrops converts a number into a string of raindrop sounds
-func Raindrops(num int) []string {
+func Raindrops(num int) string {
 	sounds := map[int]string{
 		3: "Pling",
 		5: "Plang",
@@ -20,22 +21,15 @@ func Raindrops(num int) []string {
 			raindropSounds = append(raindropSounds, sound)
 		}
 	}
-	fmt.Println(raindropSounds)
 
-	return raindropSounds
-	// switch {
-	// 	case num % 3 == 0:
-	// 		raindropSounds = append(raindropSounds, sounds[3])
-	// 		fallthrough
-	// 	case num % 5 == 0:
-	// 		raindropSounds = append(raindropSounds, sounds[3])
-	// 	case num
-	//
-	// }
+	if len(raindropSounds) == 0 {
+		return strconv.Itoa(num)
+	}
 
+	return strings.Join(raindropSounds, "")
 }
 
 func main() {
-	fmt.Println(Raindrops(28))
+	fmt.Println(Raindrops(35))
 
 }
