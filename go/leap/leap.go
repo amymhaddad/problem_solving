1f525fdf5b8ecf3a2f6a777dd9ebae18ffb59694
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-15 09:48:19 -0400

    Algo - WIP

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,7 +2,16 @@ package leap
 
 // IsLeapYear determines if a year is a leap year
 func IsLeapYear(year int) bool {
-	divisibleByFour := year%4 == 0
-	//fmt.Println("div by 4: ", divisibleByFour)
-	return divisibleByFour
+	divByFour := year%4 == 0
+	divByOneHundred := year%100 == 0
+	divByFourHundred := year%400 == 0
+
+	if !divByFour {
+		return false
+	} else if divByFour && divByOneHundred && !divByFourHundred {
+		return false
+	} else {
+		return true
+	}
+
 }
