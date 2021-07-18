00752e67037e73a7babaf52ccb7ad7b4c01c4cb6
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-18 10:48:15 -0400

    All tests pass

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -11,6 +11,12 @@ const earthSeconds = 31557600
 func Age(seconds float64, planet Planet) float64 {
 	planetOrbitalPeriod := map[Planet]float64{
 		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
 
 	orbitPeriod, ok := planetOrbitalPeriod[planet]
