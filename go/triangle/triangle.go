d90e19016aac05de473de3475d64d8240d433123
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-20 10:29:51 -0400

    Added functions to test for valid triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -16,9 +16,32 @@ const (
 
 // KindFromSides returns the kind of triangle
 func KindFromSides(a, b, c float64) Kind {
-	return "NaT"
+	validTriangle := twoSidesLargerThanThird(a, b, c)
 }
 
 func containsCorrectLengths(a, b, c float64) bool {
+	return a >= 0 && b >= 0 && c >= 0
+}
+
+func twoSidesLargerThanThird(a, b, c float64) bool {
+	//add length to this slice?
+	sides := []float64{a, b, c}
+
+	count := 0
+
+	for count < 3 {
+		s1 := 0
+		s2 := 1
+		s3 := 2
+
+		if sides[s1]+sides[s2] < sides[s3] || sides[s1]+sides[s3] < sides[s2] {
+			return false
+		}
+
+		firstNum := sides[0]
+		sides = sides[1:]
+		sides = append(sides, firstNum)
+	}
+	return true
 
 }
