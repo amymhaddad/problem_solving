65b78ddd1880a115d197b50595eab510a59058e6
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-23 09:35:17 -0400

    added function to account for inf/nan numbers

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,6 +1,8 @@
 package triangle
 
-import "fmt"
+import (
+	"math"
+)
 
 // Kind indicates the kind of triangle
 type Kind string
@@ -19,9 +21,9 @@ const (
 // KindFromSides returns the kind of triangle
 func KindFromSides(a, b, c float64) Kind {
 	validTriangle := containsCorrectLengths(a, b, c) && containsTriangleEquality(a, b, c)
+	validNumbers := containsValidNumbers(a, b, c)
 
-	if !validTriangle {
-
+	if !validTriangle || !validNumbers {
 		return "NaT"
 	}
 
@@ -29,12 +31,10 @@ func KindFromSides(a, b, c float64) Kind {
 	matches := map[float64]bool{}
 
 	for _, side := range allSides {
-		fmt.Printf("\n%T SIDE: ", side)
 		matches[side] = true
 	}
 
 	uniqueSides := len(matches)
-	//fmt.Println("\nUNIQUE: ", uniqueSides)
 	switch uniqueSides {
 	case 3:
 		return Sca
@@ -47,29 +47,37 @@ func KindFromSides(a, b, c float64) Kind {
 
 }
 
+func containsValidNumbers(a, b, c float64) bool {
+	nums := []float64{a, b, c}
+
+	for i := range nums {
+		isInf := math.IsInf(nums[i], 0)
+		nAn := math.IsNaN(nums[i])
+		if isInf || nAn {
+			return false
+		}
+	}
+
+	return true
+}
+
 func containsCorrectLengths(a, b, c float64) bool {
-	return a >= 0 && b >= 0 && c >= 0
+	return a > 0 && b > 0 && c > 0
 }
 
 func containsTriangleEquality(a, b, c float64) bool {
 
 	isValid := true
 
-	arr := []float64{3, 4, 5}
+	arr := []float64{a, b, c}
 
 	if arr[0]+arr[1] < arr[2] {
 		isValid = false
-	}
-
-	if arr[0]+arr[2] < arr[1] {
+	} else if arr[0]+arr[2] < arr[1] {
 		isValid = false
-	}
-
-	if arr[1]+arr[2] < arr[0] {
+	} else if arr[1]+arr[2] < arr[0] {
 		isValid = false
-	}
-
-	if arr[2]+arr[1] < arr[0] {
+	} else if arr[2]+arr[1] < arr[0] {
 		isValid = false
 	}
 	return isValid
