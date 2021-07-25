59e1026088c493a546a595c786f3153d6b0a70e2
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-25 08:51:33 -0400

    Completed problem

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,26 +1,16 @@
 package strand
 
-import "strings"
-
-var dnaRna = map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
+var dnaRNA = map[rune]rune{'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
 
 // ToRNA receives a dna string and returns its rna complement
 func ToRNA(dna string) string {
 
-	var rna = make([]string, len(dna))
+	var rna = make([]rune, len(dna))
 
-	for _, letter := range dna {
-		rna = append(rna, string(dnaRna[letter]))
+	for i, letter := range dna {
+		rna[i] = dnaRNA[letter]
 	}
 
-	return strings.Join(rna, "")
+	return string(rna)
+
 }
-
-// 	var rna = make([]string, len(dna))
-
-// 	for _, letter := range dna {
-// 		rna = append(rna, string(dnaRna[letter]))
-// 	}
-
-// 	return strings.Join(rna, "")
-// }
