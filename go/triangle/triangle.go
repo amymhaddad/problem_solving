3d4f6db9e3e105a298f90f4e3afbc19982a15886
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-26 07:22:53 -0400

    Updated solution

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -4,9 +4,10 @@ import (
 	"math"
 )
 
-// Kind indicates the kind of triangle
+// Kind refers to the kind of triangle that's returned by KindFromSides()
 type Kind string
 
+// Declare the constants to return kind of triangle
 const (
 	NaT = "NaT"
 	Equ = "Equ"
@@ -16,11 +17,10 @@ const (
 
 // KindFromSides returns the kind of triangle
 func KindFromSides(a, b, c float64) Kind {
-	if !ValidTriangle(a, b, c) {
-		return NaT
-	}
 
 	switch {
+	case !ValidTriangle(a, b, c):
+		return NaT
 	case a == b && b == c:
 		return Equ
 	case a == b || b == c || a == c:
@@ -31,6 +31,7 @@ func KindFromSides(a, b, c float64) Kind {
 
 }
 
+// ValidTriangle determines if a triangle is valid
 func ValidTriangle(a, b, c float64) bool {
 
 	if math.IsNaN(a+b+c) || math.IsInf(a+b+c, 0) {
@@ -47,64 +48,3 @@ func ValidTriangle(a, b, c float64) bool {
 
 	return true
 }
-
-// // KindFromSides returns the kind of triangle
-// func KindFromSides(a, b, c float64) Kind {
-// 	validTriangle := containsCorrectLengths(a, b, c) && containsTriangleEquality(a, b, c) && containsValidNumbers(a, b, c)
-
-// 	if !validTriangle {
-// 		return "NaT"
-// 	}
-
-// 	triangleSides := []float64{a, b, c}
-// 	allSides := map[float64]bool{}
-
-// 	for _, side := range triangleSides {
-// 		allSides[side] = true
-// 	}
-
-// 	uniqueSides := len(allSides)
-// 	switch uniqueSides {
-// 	case 3:
-// 		return Sca
-// 	case 2:
-// 		return Iso
-// 	default:
-// 		return Equ
-// 	}
-// }
-
-// func containsValidNumbers(a, b, c float64) bool {
-// 	nums := []float64{a, b, c}
-
-// 	for i := range nums {
-// 		if math.IsInf(nums[i], 0) || math.IsNaN(nums[i]) {
-// 			return false
-// 		}
-// 	}
-
-// 	return true
-// }
-
-// func containsCorrectLengths(a, b, c float64) bool {
-// 	return a > 0 && b > 0 && c > 0
-// }
-
-// func containsTriangleEquality(a, b, c float64) bool {
-// 	isValid := true
-
-// 	sides := []float64{a, b, c}
-
-// 	if sides[0]+sides[1] < sides[2] {
-// 		isValid = false
-// 	} else if sides[0]+sides[2] < sides[1] {
-// 		isValid = false
-// 	} else if sides[1]+sides[2] < sides[0] {
-// 		isValid = false
-// 	} else if sides[2]+sides[1] < sides[0] {
-// 		isValid = false
-// 	}
-
-// 	return isValid
-
-// }
