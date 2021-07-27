1a503b47c48f9e58d91f52d993e8608687b5e55b
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-27 07:04:41 -0400

    first commit

---
+++ go/roman-numerals/roman_numerals.go
@@ -0,0 +1,17 @@
+package romannumerals
+
+// ToRomanNumeral receives an integer and returns its Roman Numeral equivalent
+
+var arabicToRoman = map[int]string{
+	1:    "I",
+	5:    "V",
+	10:   "X",
+	50:   "L",
+	100:  "C",
+	500:  "D",
+	1000: "M",
+}
+
+func ToRomanNumeral(num int) (string, error) {
+
+}
