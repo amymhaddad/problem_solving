f6aa379548a37d4f65332b22c199bb7dc3687833
Amy M Haddad <amy.m.haddad@gmail.com>
2021-07-28 07:27:31 -0400

    Updating algorithm

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -1,58 +1,62 @@
 package romannumerals
 
-import "errors"
+import (
+	"errors"
+)
 
-var arabicToRoman = map[int]string{
-	1:    "I",
-	5:    "V",
-	10:   "X",
-	50:   "L",
-	100:  "C",
-	500:  "D",
+// ArabicToRoman contains an arabic number and its roman numeral equivalent
+// type ArabicToRoman struct {
+// 	arabic int
+// 	roman  string
+// }
+
+// var mappings = [9]ArabicToRoman{
+// 	ArabicToRoman{1000, "M"},
+// 	ArabicToRoman{500, "D"},
+// 	ArabicToRoman{100, "C"},
+// 	ArabicToRoman{50, "L"},
+// 	ArabicToRoman{10, "X"},
+// 	ArabicToRoman{9, "IV"},
+// 	ArabicToRoman{5, "V"},
+// 	ArabicToRoman{4, "IV"},
+// 	ArabicToRoman{1, "I"},
+// }
+
+var mappings = map[int]string{
 	1000: "M",
+	900:  "CM",
+	500:  "D",
+	100:  "C",
+	90:   "XC",
+	50:   "L",
+	40:   "XL",
+	10:   "X",
+	9:    "IV",
+	5:    "V",
+	4:    "IV",
+	1:    "I",
 }
 
 // ToRomanNumeral receives an integer and returns its Roman Numeral equivalent
 func ToRomanNumeral(num int) (string, error) {
 	invalidArabicNumber := num <= 0 || num > 3000
 
-	switch {
-	case invalidArabicNumber:
-		return "", errors.New("Invalid Arabic number")
-	case num < 5:
-		return underFour(num), nil
-
-	case num > 5 && num < 10:
-		return underTen(num), nil
-
-	case num > 10 && num < 50:
-		return underFifty(num), nil
-
-	default:
-		return arabicToRoman[num], nil
-	}
-}
-
-func underFour(num int) string {
-	result := ""
-	for i := 0; i < num; i++ {
-		result += arabicToRoman[1]
+	if invalidArabicNumber {
+		return "", errors.New("invalid arabic number")
 	}
 
-	if len(result) == 4 {
-		return "IV"
+	var romanLetters string
+	for num > 0 {
+		// if roman, ok := mappings[num]; ok {
+		// 	romanLetters += roman
+		// 	num -= num
+		// }
+
+		if num < 4 {
+			romanLetters += mappings[1]
+			num--
+		}
+
 	}
-
-	return result
-}
-
-func underTen(num int) string {
-	if 10-num == 1 {
-		return "IX"
-	}
-	return "VI"
-}
-
-func underFifty(num int) string {
-
+	return romanLetters, nil
 }
