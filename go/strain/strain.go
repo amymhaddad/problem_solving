6191356bef023765fb019845b44c70df55d09df4
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-07 09:29:32 -0400

    Added first method

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,5 +1,23 @@
 package strain
 
+//Ints contains a slice of one or more integers
 type Ints []int
+
+//Lists contains one or more integer slices
 type Lists [][]int
+
+//Strings contains a slice of one or more strings
 type Strings []string
+
+//Keep is a method that returns a new collection containing those elements where the predicate is true
+func (nums Ints) Keep(fn func(int) bool) []Ints {
+	var results []Ints
+
+	for _, val := range nums {
+		if fn(val) == true {
+			results = append(results, val)
+		}
+	}
+	return results
+
+}
