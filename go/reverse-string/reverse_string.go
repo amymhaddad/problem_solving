d1cd40dd727e79b1775e5bcd3837fb7fd0885fae
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-07 09:50:22 -0400

    First implementation of algo

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,1 +1,17 @@
 package reverse
+
+//Reverse receives a string and reverses it
+func Reverse(word string) string {
+	wordBytes := []byte(word)
+
+	p1 := 0
+	p2 := len(wordBytes) - 1
+
+	for p2-p1 >= 1 {
+		wordBytes[p1], wordBytes[p2] = wordBytes[p2], wordBytes[p1]
+		p2--
+		p1++
+	}
+	return string(wordBytes)
+
+}
