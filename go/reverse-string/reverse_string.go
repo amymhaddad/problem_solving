e5fcb697e358d1e9828249bf1b3e962c0553e9af
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-07 09:54:47 -0400

    Solved it

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -2,16 +2,17 @@ package reverse
 
 //Reverse receives a string and reverses it
 func Reverse(word string) string {
-	wordBytes := []byte(word)
+	runes := []rune(word)
 
 	p1 := 0
-	p2 := len(wordBytes) - 1
+	p2 := len(runes) - 1
 
 	for p2-p1 >= 1 {
-		wordBytes[p1], wordBytes[p2] = wordBytes[p2], wordBytes[p1]
+		runes[p1], runes[p2] = runes[p2], runes[p1]
 		p2--
 		p1++
 	}
-	return string(wordBytes)
+
+	return string(runes)
 
 }
