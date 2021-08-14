d25432ae68e16532f456b3285574276b030bbab9
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-14 09:31:36 -0400

    Added second function

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,34 +2,19 @@
 //sum of the squares from n to 1
 package diffsquares
 
-//SquareOfSum sums all numbers from the given number to one, and squares the
-//total
+//SquareOfSum returns the square of the sum from 1 to the given number
 func SquareOfSum(num int) int {
 	sum := num * (num + 1) / 2
 	return sum * sum
 }
 
-//SumOfSquares squares each number from the given number to one, and adds them
-//together
+//SumOfSquares returns the sum of the squares from 1 to the given number
 func SumOfSquares(num int) int {
-
-	squaredNum := num * num
-	sum := squaredNum * (squaredNum + 1) / 2
-	return sum
-	// var total int
-
-	// for num > 0 {
-	// 	total += num * num
-	// 	num--
-	// }
-	// return total
-
+	return num * (num + 1) * (2*num + 1) / 6
 }
 
-//Difference calculates the difference between the square of the sum and the sum
+//Difference returns the difference between the square of the sum and the sum
 //of the squares for a given number
 func Difference(num int) int {
-
 	return SquareOfSum(num) - SumOfSquares(num)
-
 }
