dbaf53d6c40a1018992591bb8e115208b8c57499
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-15 11:03:12 -0400

    WIP matching numbers

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,22 +1,36 @@
 package luhn
 
 import (
+	"fmt"
 	"regexp"
+	"strconv"
 )
 
-//Valid determins if a given is valid based on the Luhn formula
+//Valid determins if a given number is valid based on the Luhn formula
+//'0' is a character -- make a comparison to see if each char is >= '0' and <= '9'
 func Valid(number string) bool {
-
-	numbers := getNumbers(number)
-	if len(numbers) <= 1 {
-		return false
+	//var isNumber, _ = regexp.MatchString("[0-9]+")
+	
+		var numbers []byte
+		
+		for _, val := range number {
+			numString := string(val)
+			var isNumber, _ = regexp.MatchString("[0-9]+", numString)
+			
+			if !isNumber {
+				continue
+			}
+			updatedVal, _ := strconv.Atoi(numString)
+			numbers = append(numbers, byte(updatedVal))
+		}
+		fmt.Println(numbers)
+		return true
+	
 	}
-	return true
-}
 
-func getNumbers(number string) []string {
-	matchedNums, _ := regexp.Compile("[0-9]+")
-	found := matchedNums.FindAllString(number, -1)
-	return found
+// func getNumbers(number string) []string {
+// 	matchedNums, _ := regexp.Compile("[0-9]+")
+// 	found := matchedNums.FindAllString(number, -1)
+// 	return found
 
-}
+// }
