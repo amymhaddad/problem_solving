53c65137fced67fe54c937b79639cdc269d38901
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-18 10:39:21 -0400

    Second attempt at algorithm

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,67 +1,49 @@
 package luhn
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
 )
 
-//Valid determins if a given number is valid based on the Luhn formula
 //'0' is a character -- make a comparison to see if each char is >= '0' and <= '9'
-
+//Valid determins if a given number is valid based on the Luhn formula
 func Valid(num string) bool {
 	totalNumbers := getNumbers(num)
+
 	if totalNumbers < 1 {
 		return false
 	}
 	number := []byte(num)
-	newNum := make([]int, totalNumbers)
-
-	for i := totalNumbers - 1; i >= 0; i-- {
+	var newNums []int 
+	  
+	for i := 0; i < len(number); i++ {
 		validNums := number[i] >= '0' && number[i] <= '9'
-		// isLetter := number[i] >= 'a' && number[i] <= 'z'
-
-		// if isLetter {
-		// 	return false
-		// }
 
 		if !validNums {
 			continue
 		}
-
-		if i == 0 || i%2 == 0 {
-			n := number[i] - '0'
-			doubleNum := n * 2
-			if doubleNum > 9 {
-				doubleNum -= 9
-			}
-			newNum[i] = int(doubleNum)
-
-		} else {
-			newNum[i] = int(number[i] - '0')
-		}
-
-		fmt.Println(newNum)
-      //[8 5 6 9 0 1 8 8 7 0 0 3 8 3 0 6]
-
-
-		// }
-		// 	if validNums {
-		// 		newNum = append(newNum, int(number[i]-'0'))
-		// 	}
-		// }
-		// fmt.Println(newNum)
-
-		// reversed := make([]int, len(newNum))
-
-		// counter := 0
-		// for i := len(newNum) - 1; i >= 0; i-- {
-		// 	reversed[i] = newNum[counter]
-		// 	counter++
-		// }
-		// fmt.Println(reversed)
+		newNums = append(newNums, int(number[i] - '0'))
 	}
-	return true
+	
+	luhnNums := make([]int, len(newNums))
+	var totalSum int
+	for i := len(newNums) -1; i >= 0; i-- {
+		if i == 0 || i % 2 == 0 {
+			doubleNum := newNums[i] * 2
+			if doubleNum > 9{
+				doubleNum -= 9
+			} else {
+				luhnNums[i] = doubleNum
+			}
+			totalSum += doubleNum
+		} else {
+			luhnNums[i] = newNums[i]
+			totalSum += newNums[i]
+		}
+		
+	}
+
+	return totalSum % 10 == 0
 }
 
 func getNumbers(number string) int {
@@ -69,18 +51,3 @@ func getNumbers(number string) int {
 	found := matchedNums.FindAllString(number, -1)
 	return len(strings.Join(found, ""))
 }
-
-func main() {
-	x := "4539 1488 0343 6467"
-	y := Valid(x)
-	//y := Valid(x)
-	fmt.Println(y)
-}
-
-
-// func getNumbers(number string) []string {
-// 	matchedNums, _ := regexp.Compile("[0-9]+")
-// 	found := matchedNums.FindAllString(number, -1)
-// 	return found
-
-// }
