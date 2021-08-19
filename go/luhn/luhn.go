fc075bb0bbc7e21802b5c4ca8593eae9e6a8a842
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-19 10:17:38 -0400

    Refactoring - pulled out 2nd function to get valid numbers

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -8,40 +8,19 @@ import (
 //Valid determins if a given number is valid based on the Luhn formula
 func Valid(num string) bool {
 
-	// num = strings.ReplaceAll(num, " ", "")
-
-	// if len(num) <= 1 || (num[len(num)-1] >= 97 && num[len(num)-1] <= 122) {
-	// 	return false
-	// }
-
-	if !isValid(num) {
+	if !isValidString(num) {
 		return false
 	}
 
-	number := []byte(num)
-	var newNums []int
+	validNumbers := getValidNums(num)
 
-	for i := 0; i < len(number); i++ {
-		validNums := number[i] >= '0' && number[i] <= '9'
-		if validNums {
-			newNums = append(newNums, int(number[i]-'0'))
-		}
-
-		// if !validNums {
-		// 	return false
-		// }
-		//This works bc only looking for nums 0-9.
-		//THis won't work for other chars bc it'll exeed the amount
-		//newNums = append(newNums, int(number[i]-'0'))
-	}
-
-	luhnNums := make([]int, len(newNums))
+	luhnNums := make([]int, len(validNumbers))
 	var totalSum int
-	indexToDouble := len(newNums) - 2
+	indexToDouble := len(validNumbers) - 2
 
-	for i := len(newNums) - 1; i >= 0; i-- {
+	for i := len(validNumbers) - 1; i >= 0; i-- {
 		if i == indexToDouble && indexToDouble >= 0 {
-			doubleNum := newNums[indexToDouble] * 2
+			doubleNum := validNumbers[indexToDouble] * 2
 			if doubleNum > 9 {
 				doubleNum -= 9
 			} else {
@@ -50,8 +29,8 @@ func Valid(num string) bool {
 			totalSum += doubleNum
 			indexToDouble -= 2
 		} else {
-			luhnNums[i] = newNums[i]
-			totalSum += newNums[i]
+			luhnNums[i] = validNumbers[i]
+			totalSum += validNumbers[i]
 		}
 
 	}
@@ -59,7 +38,7 @@ func Valid(num string) bool {
 	return totalSum%10 == 0
 }
 
-func isValid(num string) bool {
+func isValidString(num string) bool {
 	num = strings.ReplaceAll(num, " ", "")
 
 	if len(num) <= 1 || (num[len(num)-1] >= 97 && num[len(num)-1] <= 122) {
@@ -77,4 +56,17 @@ func isValid(num string) bool {
 	}
 	return true
 
+}
+
+func getValidNums(num string) []int {
+	number := []byte(num)
+	var validNumbers []int
+
+	for i := 0; i < len(number); i++ {
+		validNums := number[i] >= '0' && number[i] <= '9'
+		if validNums {
+			validNumbers = append(validNumbers, int(number[i]-'0'))
+		}
+	}
+	return validNumbers
 }
