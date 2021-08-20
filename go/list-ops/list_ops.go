a8ba93afd6bad630c3bf8aea615460c32b0529d1
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-20 11:00:25 -0400

    First commit

---
+++ go/list-ops/list_ops.go
@@ -0,0 +1,10 @@
+package listops
+
+type IntList []int
+
+
+//inner function takes 2 ints and returns an int
+//the entire method returns an int
+func (nums IntList) Foldl(fn func (int, int) int) int {
+
+}
