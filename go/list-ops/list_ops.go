d331dfb4c9e185e24684cb46cce92a4c98e758bd
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-20 14:07:25 -0400

    Added 2 methods

--- go/list-ops/list_ops.go
+++ go/list-ops/list_ops.go
@@ -2,9 +2,21 @@ package listops
 
 type IntList []int
 
+//always look at the test to determine what to write (ie, function or method)
+// adding behavior to IntList type
+
+//a function can be a type -- just naming something particular
+//type is binFunc; func(type of params) return type
+type binFunc func(int, int) int
 
 //inner function takes 2 ints and returns an int
 //the entire method returns an int
-func (nums IntList) Foldl(fn func (int, int) int) int {
+//param name is fn and the type binFund
+func (nums IntList) Foldl(fn binFunc, initial int) int {
+	return 2
+}
 
+func (nums IntList) Foldr(fn binFunc, inital int) int {
+
+	return 2
 }
