fd6752f3871a8bb679b73d12e96aa1d645166a6f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-22 10:13:32 -0400

    Added filter method

--- go/list-ops/list_ops.go
+++ go/list-ops/list_ops.go
@@ -9,6 +9,8 @@ type IntList []int
 //type is binFunc; func(type of params) return type
 type binFunc func(int, int) int
 
+type predFunc func(int) bool
+
 //inner function takes 2 ints and returns an int
 //the entire method returns an int
 //param name is fn and the type binFund
@@ -40,4 +42,21 @@ func (nums IntList) Foldr(fn binFunc, initial int) int {
 	}
 	return total
 
+}
+
+//if use IntList in the program, must use {} to initialize it
+//It's a method on IntList, so I'm probably going to do something w/it and
+//return it
+func (nums IntList) Filter(fn predFunc) IntList {
+	filteredNums := IntList{}
+	if len(nums) == 0 {
+		return filteredNums
+	}
+
+	for _, value := range nums {
+		if fn(value) {
+			filteredNums = append(filteredNums, value)
+		}
+	}
+	return filteredNums
 }
