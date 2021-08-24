8ce8b2258812dbf48cc12e915dd6b7908fbd68bb
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-24 10:15:05 -0400

    First method works - KeepInts

--- go/strain/strain.go
+++ go/strain/strain.go
@@ -1,64 +1,23 @@
 package strain
 
-//Ints contains a slice of one or more integers
+//Ints contains a slice of ints
 type Ints []int
 
-//Lists contains one or more integer slices
+//Lists contains a slice of slices
 type Lists [][]int
 
-//Strings contains a slice of one or more strings
+//Strings contains a slice of strings
 type Strings []string
 
-//Keep filters integers and returns a new collection containing those elements where the predicate is true
 func (nums Ints) Keep(fn func(int) bool) Ints {
 	var result Ints
 
-	for _, num := range nums {
-		if fn(num) {
-			result = append(result, num)
-		}
-	}
-
-	return result
-
-}
-
-//Discard filters integers and returns a new collection containing those elements where the predicate is false.
-func (nums Ints) Discard(fn func(int) bool) Ints {
-	var result Ints
-
-	for _, num := range nums {
-		if !fn(num) {
-			result = append(result, num)
-		}
-	}
-
-	return result
-
-}
-
-//Keep filters strings and returns a new collection containing those elements where the predicate is true
-func (words Strings) Keep(fn func(string) bool) Strings {
-	var result Strings
-
-	for _, word := range words {
-		if fn(word) {
-			result = append(result, word)
+	for _, val := range nums {
+		if fn(val) {
+			result = append(result, val)
 		}
 	}
 
 	return result
 }
 
-//Keep filters slices and returns a new collection containing those elements where the predicate is true
-func (lists Lists) Keep(fn func([]int) bool) Lists {
-	var result Lists
-
-	for _, list := range lists {
-		if fn(list) {
-			result = append(result, list)
-		}
-	}
-
-	return result
-}
