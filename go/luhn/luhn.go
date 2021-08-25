67d71e1479d77a599f29418b6b07bae4b2c607f8
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-25 10:07:54 -0400

    First commit

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -3,76 +3,29 @@ package luhn
 
 import (
 	"strings"
+	"unicode"
 )
 
 //Valid determines if a given number is valid based on the Luhn formula
-func Valid(num string) bool {
+func Valid(nums string) bool {
+	nums = strings.ReplaceAll(nums, " ", "")
+	altDigit := len(nums) - 2
 
-	if !isValidString(num) {
-		return false
-	}
+	var total int
 
-	validNumbers := getValidNums(num)
-
-	return isLuhn(validNumbers)
-
-}
-
-func isValidString(num string) bool {
-	num = strings.ReplaceAll(num, " ", "")
-	letterChar := num[len(num)-1] >= 97 && num[len(num)-1] <= 122
-
-	if len(num) <= 1 || letterChar {
-		return false
-	}
-
-	for i := 0; i < len(num); i++ {
-		validNums := num[i] >= '0' && num[i] <= '9'
-
-		if !validNums {
+	for i, val := range nums {
+		if !unicode.IsNumber(val) || len(nums) <= 1 {
 			return false
 		}
-	}
 
-	return true
-
-}
-
-func getValidNums(num string) []int {
-	var validNumbers []int
-
-	for i := 0; i < len(num); i++ {
-		validNums := num[i] >= '0' && num[i] <= '9'
-		if validNums {
-			validNumbers = append(validNumbers, int(num[i]-'0'))
+		digitValue := int(val - '0')
+		if i == altDigit {
+			digitValue = (digitValue * 2) % 9
+			altDigit -= 2
 		}
+		total += digitValue
 	}
 
-	return validNumbers
+	return total%10 == 0
 
-}
-
-func isLuhn(validNumbers []int) bool {
-	luhnNums := make([]int, len(validNumbers))
-	var totalSum int
-	indexToDouble := len(validNumbers) - 2
-
-	for i := len(validNumbers) - 1; i >= 0; i-- {
-		if i == indexToDouble && indexToDouble >= 0 {
-			doubleNum := validNumbers[indexToDouble] * 2
-			if doubleNum > 9 {
-				doubleNum -= 9
-			}
-
-			luhnNums[i] = doubleNum
-			totalSum += doubleNum
-			indexToDouble -= 2
-
-		} else {
-			luhnNums[i] = validNumbers[i]
-			totalSum += validNumbers[i]
-		}
-	}
-
-	return totalSum%10 == 0
 }
