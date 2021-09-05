b8b574e31177b9bce02fb64e0a51bb0c7120143e
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-05 07:32:14 -0400

    Refactored algorithm and all tests pass

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -2,9 +2,7 @@
 package luhn
 
 import (
-	"fmt"
 	"strings"
-	"unicode"
 )
 
 //Valid determines if a given number is valid based on the Luhn formula
@@ -13,48 +11,24 @@ func Valid(nums string) bool {
 	double := false
 	var total int
 
-	//reg for loop bc going right ot left
-	for _, val := range nums {
-		if !unicode.IsNumber(val) || len(nums) <= 1 {
+	for i := len(nums) - 1; i >= 0; i-- {
+		validNum := nums[i] >= 48 && nums[i] <= 57
+
+		if !validNum || len(nums) <= 1 {
 			return false
 		}
 
-		digitValue := int(val - '0')
-		fmt.Println(digitValue)
-		if double && digitValue != 9 {
-			digitValue = (digitValue * 2) % 9
+		digit := int(nums[i] - 48)
+
+		if double && digit != 9 {
+			digit = (digit * 2) % 9
 			double = false
 		} else {
 			double = true
 		}
 
-		total += digitValue
+		total += digit
 	}
 	return total%10 == 0
 
 }
-
-// func Valid(nums string) bool {
-// 	nums = strings.ReplaceAll(nums, " ", "")
-// 	altDigit := len(nums) - 2
-// 	var total int
-
-// 	for i, val := range nums {
-// 		if !unicode.IsNumber(val) || len(nums) <= 1 {
-// 			return false
-// 		}
-// 		currIndex := len(nums) - 1 - i
-
-// 		currRuneVal := nums[currIndex]
-// 		fmt.Println("here: ", currRuneVal)
-// 		digitValue := int(val - '0')
-
-// 		if currIndex == altDigit && digitValue != 9 {
-// 			digitValue = (digitValue * 2) % 9
-// 			altDigit -= 2
-// 		}
-// 		total += digitValue
-
-// 	}
-// 	return total%10 == 0
-// }
