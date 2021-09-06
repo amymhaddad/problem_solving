32af6cdb06b3923edc0786a1421857519e592058
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-06 09:53:52 -0400

    Added new algorithm

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -8,27 +8,28 @@ import (
 //Valid determines if a given number is valid based on the Luhn formula
 func Valid(nums string) bool {
 	nums = strings.ReplaceAll(nums, " ", "")
-	double := false
+	double := len(nums)%2 == 0
 	var total int
 
-	for i := len(nums) - 1; i >= 0; i-- {
-		validNum := nums[i] >= 48 && nums[i] <= 57
-
-		if !validNum || len(nums) <= 1 {
+	if len(nums) <= 1 {
+		return false
+	}
+	for _, num := range nums {
+		validNum := num-'0' >= 0 && num-'0' <= 9
+		if !validNum {
 			return false
 		}
 
-		digit := int(nums[i] - 48)
+		digit := int(num - '0')
 
 		if double && digit != 9 {
 			digit = (digit * 2) % 9
-			double = false
-		} else {
-			double = true
 		}
 
+		double = !double
 		total += digit
 	}
+
 	return total%10 == 0
 
 }
