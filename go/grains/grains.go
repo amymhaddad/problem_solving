e3aba52f5327aa48d7b67e07cdfa15a727e4e52d
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-12 07:55:36 -0400

    4 tests pass

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,13 +2,34 @@ package grains
 
 import "errors"
 
+//Square calculates the number of grains on a given square
 func Square(num int) (uint64, error) {
 	if num <= 0 || num > 64 {
 		return 0, errors.New("Invalid number")
 	}
 
-	if num <= 2 {
-		return uint64(num), nil
+	// if num <= 2 {
+	// 	return uint64(num), nil
+	// }
+	//
+	// total := 1
+	// for i := 2; i <= num; i++ {
+	// 	total += i * 2
+	// }
+	total := 1
+
+	if num == 1 {
+		return uint64(total), nil
 	}
 
+	for i := 2; i <= num; i++ {
+		value := total * 2
+		total = value
+	}
+
+	return uint64(total), nil
+	// if num <= 2 {
+	// 	return uint64(num), nil
+	// }
+	//
 }
