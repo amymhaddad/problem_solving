0d3776e2630c9abbfa170afe47885af10df334f8
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-13 07:19:29 -0400

    Solved it

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -9,10 +9,10 @@ func Welcome(name string) string {
 
 // HappyBirthday wishes happy birthday to the birthday person and stands out his age.
 func HappyBirthday(name string, age int) string {
-	panic("Please implement the HappyBirthday function")
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbour, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	return fmt.Sprintf("Welcome to my party, %s!\nYou have been assigned to table %dB. Your table is %s, exactly %.1f meters from here.\nYou will be sitting next to %s.", name, table, direction, distance, neighbour)
 }
