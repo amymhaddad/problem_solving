582e1480c7b7f4bae8277803119e559c0b1b52ed
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-22 10:01:09 -0400

    Solved it

--- go/need-for-speed/need_for_speed.go
+++ go/need-for-speed/need_for_speed.go
@@ -8,33 +8,41 @@ type Car struct {
 	distance     int
 }
 
+// Track implements a racetrack
+type Track struct {
+	distance int
+}
+
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
 	return Car{
 		speed:        speed,
 		batteryDrain: batteryDrain,
 		battery:      100,
-		distance:     0,
 	}
-}
-
-// Track implements a race track.
-type Track struct {
-	distance int
 }
 
 // NewTrack created a new track
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+	return Track{
+		distance: distance,
+	}
 }
 
 // Drive drives the car one time. If there is not enough battery to drive on more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+
+	return car
+
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+	batteryAmount := car.battery / car.batteryDrain
+	return batteryAmount*car.speed >= track.distance
 }
--- go/need-for-speed/need_for_speed_test.go
+++ go/need-for-speed/need_for_speed_test.go
@@ -35,147 +35,147 @@ func TestNewCar(t *testing.T) {
 
 }
 
-// func TestNewTrack(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		track    Track
-// 		expected Track
-// 	}{
-// 		{
-// 			name: "Create a new track.",
-// 			track: Track{
-// 				distance: 800,
-// 			},
-// 			expected: Track{
-// 				distance: 800,
-// 			},
-// 		},
-// 		{
-// 			name: "Create a new track.",
-// 			track: Track{
-// 				distance: 360,
-// 			},
-// 			expected: Track{
-// 				distance: 360,
-// 			},
-// 		},
-// 	}
+func TestNewTrack(t *testing.T) {
+	tests := []struct {
+		name     string
+		track    Track
+		expected Track
+	}{
+		{
+			name: "Create a new track.",
+			track: Track{
+				distance: 800,
+			},
+			expected: Track{
+				distance: 800,
+			},
+		},
+		{
+			name: "Create a new track.",
+			track: Track{
+				distance: 360,
+			},
+			expected: Track{
+				distance: 360,
+			},
+		},
+	}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := NewTrack(tt.track.distance)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewTrack(tt.track.distance)
 
-// 			if got != tt.expected {
-// 				t.Errorf("NewTrack(%v) = %v; expected %v", tt.track.distance, tt.track, tt.expected)
-// 			}
-// 		})
-// 	}
-// }
+			if got != tt.expected {
+				t.Errorf("NewTrack(%v) = %v; expected %v", tt.track.distance, tt.track, tt.expected)
+			}
+		})
+	}
+}
 
-// func TestDrive(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		car      Car
-// 		expected Car
-// 	}{
-// 		{
-// 			name: "Drive the car with full battery.",
-// 			car: Car{
-// 				speed:        5,
-// 				batteryDrain: 2,
-// 				battery:      100,
-// 				distance:     0,
-// 			},
-// 			expected: Car{
-// 				speed:        5,
-// 				batteryDrain: 2,
-// 				battery:      98,
-// 				distance:     5,
-// 			},
-// 		},
-// 		{
-// 			name: "Drive the car with battery drained.",
-// 			car: Car{
-// 				speed:        5,
-// 				batteryDrain: 5,
-// 				battery:      4,
-// 				distance:     0,
-// 			},
-// 			expected: Car{
-// 				speed:        5,
-// 				batteryDrain: 5,
-// 				battery:      4,
-// 				distance:     0,
-// 			},
-// 		},
-// 	}
+func TestDrive(t *testing.T) {
+	tests := []struct {
+		name     string
+		car      Car
+		expected Car
+	}{
+		{
+			name: "Drive the car with full battery.",
+			car: Car{
+				speed:        5,
+				batteryDrain: 2,
+				battery:      100,
+				distance:     0,
+			},
+			expected: Car{
+				speed:        5,
+				batteryDrain: 2,
+				battery:      98,
+				distance:     5,
+			},
+		},
+		{
+			name: "Drive the car with battery drained.",
+			car: Car{
+				speed:        5,
+				batteryDrain: 5,
+				battery:      4,
+				distance:     0,
+			},
+			expected: Car{
+				speed:        5,
+				batteryDrain: 5,
+				battery:      4,
+				distance:     0,
+			},
+		},
+	}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := Drive(tt.car)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Drive(tt.car)
 
-// 			if got != tt.expected {
-// 				t.Errorf("NewCar(%v) = %v; expected %v", tt.car, got, tt.expected)
-// 			}
-// 		})
-// 	}
-// }
+			if got != tt.expected {
+				t.Errorf("NewCar(%v) = %v; expected %v", tt.car, got, tt.expected)
+			}
+		})
+	}
+}
 
-// func TestCanFinish(t *testing.T) {
-// 	tests := []struct {
-// 		name     string
-// 		car      Car
-// 		track    Track
-// 		expected bool
-// 	}{
-// 		{
-// 			name: "Car can easily finish the track distance.",
-// 			car: Car{
-// 				speed:        5,
-// 				batteryDrain: 2,
-// 				battery:      100,
-// 				distance:     20,
-// 			},
-// 			track: Track{
-// 				distance: 30,
-// 			},
-// 			expected: true,
-// 		},
-// 		{
-// 			name: "Car can finish the track distance just before battery drained.",
-// 			car: Car{
-// 				speed:        5,
-// 				batteryDrain: 5,
-// 				battery:      100,
-// 				distance:     20,
-// 			},
-// 			track: Track{
-// 				distance: 20,
-// 			},
-// 			expected: true,
-// 		},
-// 		{
-// 			name: "Car cannot finish the track distance.",
-// 			car: Car{
-// 				speed:        2,
-// 				batteryDrain: 10,
-// 				battery:      100,
-// 				distance:     5,
-// 			},
-// 			track: Track{
-// 				distance: 80,
-// 			},
-// 			expected: false,
-// 		},
-// 	}
+func TestCanFinish(t *testing.T) {
+	tests := []struct {
+		name     string
+		car      Car
+		track    Track
+		expected bool
+	}{
+		{
+			name: "Car can easily finish the track distance.",
+			car: Car{
+				speed:        5,
+				batteryDrain: 2,
+				battery:      100,
+				distance:     20,
+			},
+			track: Track{
+				distance: 30,
+			},
+			expected: true,
+		},
+		{
+			name: "Car can finish the track distance just before battery drained.",
+			car: Car{
+				speed:        5,
+				batteryDrain: 5,
+				battery:      100,
+				distance:     20,
+			},
+			track: Track{
+				distance: 20,
+			},
+			expected: true,
+		},
+		{
+			name: "Car cannot finish the track distance.",
+			car: Car{
+				speed:        2,
+				batteryDrain: 10,
+				battery:      100,
+				distance:     5,
+			},
+			track: Track{
+				distance: 80,
+			},
+			expected: false,
+		},
+	}
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := CanFinish(tt.car, tt.track)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := CanFinish(tt.car, tt.track)
 
-// 			if got != tt.expected {
-// 				t.Errorf("NewCar(%v, %v) = %v; expected %v", tt.car, tt.track, got, tt.expected)
-// 			}
-// 		})
-// 	}
-// }
+			if got != tt.expected {
+				t.Errorf("NewCar(%v, %v) = %v; expected %v", tt.car, tt.track, got, tt.expected)
+			}
+		})
+	}
+}
