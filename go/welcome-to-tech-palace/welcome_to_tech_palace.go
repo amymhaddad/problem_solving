23e927d5eec7705ac63a432e5a291297cc83cd80
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-28 06:38:07 -0400

    Refactoring

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -7,20 +7,16 @@ import (
 
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	var greeting string
-	greeting = strings.Repeat("*", numStarsPerLine)
-	greeting += "\n" + welcomeMsg + "\n"
-	greeting += strings.Repeat("*", numStarsPerLine)
-	return greeting
+	repeatStars := strings.Repeat("*", numStarsPerLine)
+	return fmt.Sprintf("%s\n%s\n%s", repeatStars, welcomeMsg, repeatStars)
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	oldMsg = strings.ReplaceAll(oldMsg, "*", "")
-	return strings.TrimSpace(oldMsg)
+	return strings.Trim(oldMsg, "* \n")
 }
