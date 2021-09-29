5ce56716ddf9e0a805cfe2df0413db23edd8da7a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-29 10:17:59 -0400

    Second function passes

--- go/logs-logs-logs/logs.go
+++ go/logs-logs-logs/logs.go
@@ -3,20 +3,20 @@ package logs
 import (
 	"regexp"
 	"strings"
+	"unicode/utf8"
 )
 
 // Message extracts the message from the provided log line.
 func Message(line string) string {
 	re := regexp.MustCompile(`\[[A-Z]+\]:\s*`)
 	result := re.FindStringSubmatch(line)
-
 	line = strings.ReplaceAll(line, result[0], "")
 	return strings.TrimSpace(line)
 }
 
 // MessageLen counts the amount of characters (runes) in the message of the log line.
 func MessageLen(line string) int {
-	panic("Please implement the MessageLen() function")
+	return utf8.RuneCountInString(Message(line))
 }
 
 // LogLevel extracts the log level string from the provided log line.
