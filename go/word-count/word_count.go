f23bb87eb0a7bb2cafcc77318b2d6df163a430ed
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-20 15:30:53 -0400

    Updated algo

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,6 +1,7 @@
 package wordcount
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -12,16 +13,25 @@ type Frequency map[string]int
 func WordCount(s string) Frequency {
 	//always need to init a map which is what make() does.
 	f := make(Frequency)
+	//f := Frequency{} -- less common - use when i have map vals I want to init w/
 	s = strings.ToLower(s)
 
-	re := regexp.MustCompile("[A-Za-z]*['a-z]?[0-9]*")
+	//match an apostrophe and any letter w/in the range [a-z] as a group
+	//Use group -- not just []. If put apostrophe inside brackets ['a-z]? -- this matches any of these chars
+	//vs what I have now is a specifc order
+	re := regexp.MustCompile("[0-9A-Za-z]+('[a-z])?")
+
+	//Works in Python, not Go. unclear how to capture numbers and letters
+	//re := regexp.MustCompile("\d*\w['a-z]?")
 	words := re.FindAllString(s, -1)
+	fmt.Println("words: ", words, len(words))
 
 	for _, word := range words {
 		count, found := f[word]
 
 		if found {
-			count++
+			//	fmt.Println("found", word, count)
+			f[word] = count + 1
 		} else {
 			f[word] = 1
 		}
