5c5fe5e846bce79a42665b52f4194f542c4ffdf4
Amy M Haddad <amy.m.haddad@gmail.com>
2021-11-09 09:29:03 -0500

    first commit

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,7 +1,6 @@
 package robotname
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
@@ -13,41 +12,18 @@ type Robot struct {
 
 var usedNames = make(map[string]bool)
 
-const nameLength = 5
-
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-//Name returns a new Robot name
-func (r *Robot) Name() (string, error) {
-	if r.name != "" {
-		return r.name, nil
-	}
+//SHould this be Name or getName()
+//Name creates a robot name
+func (r *Robot) Name() string {
 
 	name := getName()
 
-	for {
-		_, found := usedNames[name]
-		if found || len(name) < 5 {
-			name = getName()
-			continue
-		}
-		usedNames[name] = true
-		r.name = name
-		break
-	}
-	return name, nil
-}
-
-//Reset resets a robot's name to be empty
-func (r *Robot) Reset() {
-	r.name = ""
 }
 
 func getName() string {
-	ch1 := rand.Intn(26) + 'A'
-	ch2 := rand.Intn(26) + 'A'
-	digits := rand.Intn(1000)
-	return fmt.Sprintf("%c%c%d", ch1, ch2, digits)
+
 }
