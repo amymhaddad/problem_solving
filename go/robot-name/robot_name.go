30b53e00244cedb8b55429c10554b6722332b211
Amy M Haddad <amy.m.haddad@gmail.com>
2021-11-09 09:39:10 -0500

    First test passes

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,6 +1,7 @@
 package robotname
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -12,18 +13,34 @@ type Robot struct {
 
 var usedNames = make(map[string]bool)
 
+const nameLength = 5
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 //SHould this be Name or getName()
 //Name creates a robot name
-func (r *Robot) Name() string {
+func (r *Robot) Name() (string, error) {
 
 	name := getName()
+
+	_, found := usedNames[name]
+
+	for found || len(name) < nameLength {
+		name = getName()
+	}
+	usedNames[name] = true
+	fmt.Println(name)
+	return name, nil
 
 }
 
 func getName() string {
+	ch1 := rand.Intn(26) + 'A'
+	ch2 := rand.Intn(26) + 'A'
+	digits := rand.Intn(1000)
+	fmt.Println("chrs", ch1, digits)
+	return fmt.Sprintf("%c%c%d", ch1, ch2, digits)
 
 }
