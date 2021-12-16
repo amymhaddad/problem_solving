324c2e7a9dff7e6ff84215bc159ca9e312028b4a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-16 07:57:25 -0500

    Added switch statement

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,6 +1,7 @@
 package rotationalcipher
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -14,28 +15,48 @@ func RotationalCipher(str string, shiftKey int) string {
 		var distFromA int
 		var newVal rune
 
-		if strings.ContainsRune(string(val), 32) {
+		switch {
+		case strings.ContainsRune(string(val), 32):
 			newString = append(newString, ' ')
 			continue
-		}
-
-		if strings.ContainsAny(string(val), "0123456789") {
+		case strings.ContainsAny(string(val), "0123456789"):
 			newString = append(newString, val)
 			continue
-
-		}
-
-		if int(val) >= 65 && int(val) <= 90 {
+		case int(val) >= 65 && int(val) <= 90:
 			distFromA = int(val) - int('A')
 			shiftVal = (distFromA + shiftKey) % alphaLength
 			newVal = rune(int('A') + shiftVal)
-		} else {
+		case int(val) >= 97 && int(val) <= 123:
 			distFromA = int(val) - int('a')
 			shiftVal = (distFromA + shiftKey) % alphaLength
 			newVal = rune(int('a') + shiftVal)
+		default:
+			newString = append(newString, val)
 		}
 		newString = append(newString, newVal)
+		// if strings.ContainsRune(string(val), 32) {
+		// 	newString = append(newString, ' ')
+		// 	continue
+		// }
+		//
+		// if strings.ContainsAny(string(val), "0123456789") {
+		// 	newString = append(newString, val)
+		// 	continue
+		//
+		// }
+		//
+		// if int(val) >= 65 && int(val) <= 90 {
+		// 	distFromA = int(val) - int('A')
+		// 	shiftVal = (distFromA + shiftKey) % alphaLength
+		// 	newVal = rune(int('A') + shiftVal)
+		// } else {
+		// 	distFromA = int(val) - int('a')
+		// 	shiftVal = (distFromA + shiftKey) % alphaLength
+		// 	newVal = rune(int('a') + shiftVal)
+		// }
+		//	newString = append(newString, newVal)
 
 	}
+	fmt.Println("H: ", string(newString))
 	return string(newString)
 }
