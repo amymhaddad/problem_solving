b3b194cbb2f15810164119b188c706dca799c356
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-16 10:15:03 -0500

    Solved it

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,62 +1,38 @@
 package rotationalcipher
 
 import (
-	"fmt"
 	"strings"
 )
 
 const alphaLength = 26
 
+//RotationalCipher is a shift cipher that transposes the letters in the alphabet using an integer key between 0 and 26
 func RotationalCipher(str string, shiftKey int) string {
 	var newString []rune
 
 	for _, val := range str {
 		var shiftVal int
-		var distFromA int
-		var newVal rune
+		var distFromStartOfAlpha int
+		var rotatedVal rune
 
 		switch {
 		case strings.ContainsRune(string(val), 32):
 			newString = append(newString, ' ')
-			continue
 		case strings.ContainsAny(string(val), "0123456789"):
 			newString = append(newString, val)
-			continue
 		case int(val) >= 65 && int(val) <= 90:
-			distFromA = int(val) - int('A')
-			shiftVal = (distFromA + shiftKey) % alphaLength
-			newVal = rune(int('A') + shiftVal)
+			distFromStartOfAlpha = int(val) - int('A')
+			shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+			rotatedVal = rune(int('A') + shiftVal)
+			newString = append(newString, rotatedVal)
 		case int(val) >= 97 && int(val) <= 123:
-			distFromA = int(val) - int('a')
-			shiftVal = (distFromA + shiftKey) % alphaLength
-			newVal = rune(int('a') + shiftVal)
+			distFromStartOfAlpha = int(val) - int('a')
+			shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+			rotatedVal = rune(int('a') + shiftVal)
+			newString = append(newString, rotatedVal)
 		default:
 			newString = append(newString, val)
 		}
-		newString = append(newString, newVal)
-		// if strings.ContainsRune(string(val), 32) {
-		// 	newString = append(newString, ' ')
-		// 	continue
-		// }
-		//
-		// if strings.ContainsAny(string(val), "0123456789") {
-		// 	newString = append(newString, val)
-		// 	continue
-		//
-		// }
-		//
-		// if int(val) >= 65 && int(val) <= 90 {
-		// 	distFromA = int(val) - int('A')
-		// 	shiftVal = (distFromA + shiftKey) % alphaLength
-		// 	newVal = rune(int('A') + shiftVal)
-		// } else {
-		// 	distFromA = int(val) - int('a')
-		// 	shiftVal = (distFromA + shiftKey) % alphaLength
-		// 	newVal = rune(int('a') + shiftVal)
-		// }
-		//	newString = append(newString, newVal)
-
 	}
-	fmt.Println("H: ", string(newString))
 	return string(newString)
 }
