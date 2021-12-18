22267936b93a4627ee719623d750f43dd648c28f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-18 07:23:32 -0500

    Extracted rotated value logic into function

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -2,6 +2,7 @@ package rotationalcipher
 
 import (
 	"strings"
+	"unicode"
 )
 
 const alphaLength = 26
@@ -11,25 +12,24 @@ func RotationalCipher(str string, shiftKey int) string {
 	var newVals strings.Builder
 
 	for _, val := range str {
-		var shiftVal int
-		var distFromStartOfAlpha int
-		var rotatedVal rune
-
 		switch {
 		case strings.ContainsRune(string(val), 32):
 			newVals.WriteRune(' ')
 		case strings.ContainsAny(string(val), "0123456789"):
 			newVals.WriteRune(val)
-		case int(val) >= 65 && int(val) <= 90:
-			distFromStartOfAlpha = int(val) - int('A')
-			shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
-			rotatedVal = rune(int('A') + shiftVal)
+		case unicode.IsUpper(val):
+			rotatedVal := rotateLetter(val, shiftKey)
+			// distFromStartOfAlpha = int(val) - int('A')
+			// shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+			// rotatedVal = rune(int('A') + shiftVal)
 			newVals.WriteRune(rotatedVal)
 
-		case int(val) >= 97 && int(val) <= 123:
-			distFromStartOfAlpha = int(val) - int('a')
-			shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
-			rotatedVal = rune(int('a') + shiftVal)
+		case unicode.IsLower(val):
+
+			rotatedVal := rotateLetter(val, shiftKey)
+			// distFromStartOfAlpha = int(val) - int('a')
+			// shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+			// rotatedVal = rune(int('a') + shiftVal)
 			newVals.WriteRune(rotatedVal)
 
 		default:
@@ -37,6 +37,25 @@ func RotationalCipher(str string, shiftKey int) string {
 		}
 	}
 	return newVals.String()
+}
+
+//Add bool as aparm on refactor
+func rotateLetter(letter rune, shiftKey int) rune {
+	var distFromStartOfAlpha int
+	var shiftVal int
+	var rotatedVal rune
+
+	if unicode.IsUpper(letter) {
+		distFromStartOfAlpha = int(letter) - int('A')
+		shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+		rotatedVal = rune(int('A') + shiftVal)
+	} else {
+		distFromStartOfAlpha = int(letter) - int('a')
+		shiftVal = (distFromStartOfAlpha + shiftKey) % alphaLength
+		rotatedVal = rune(int('a') + shiftVal)
+	}
+	return rotatedVal
+
 }
 
 // func RotationalCipher(str string, shiftKey int) string {
