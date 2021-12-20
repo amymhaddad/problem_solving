022ce1eb7946a10513af7b0a8a25c4254b754216
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-20 10:15:10 -0500

    Updating var names

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -11,27 +11,22 @@ const alphaLength = 26
 func RotationalCipher(str string, shiftKey int) string {
 	var result strings.Builder
 
-	for _, val := range str {
+	for _, r := range str {
 		var letterSize int
-		isLower := unicode.IsLower(val)
-		isUpper := unicode.IsUpper(val)
-		isNumber := strings.ContainsAny(string(val), "0123456789")
-		isSpace := strings.ContainsRune(string(val), 32)
 
-		switch {
-		case isSpace:
-			result.WriteRune(' ')
-		case isNumber || (!isUpper && !isLower):
-			result.WriteRune(val)
-		default:
-			if isUpper {
-				letterSize = int('A')
-			} else {
-				letterSize = int('a')
-			}
-			rotatedVal := rotateLetter(val, shiftKey, letterSize)
-			result.WriteRune(rotatedVal)
+		if !unicode.IsLetter(r) {
+			result.WriteRune(r)
+			continue
 		}
+
+		if unicode.IsUpper(r) {
+			letterSize = int('A')
+		} else {
+			letterSize = int('a')
+		}
+		rotatedVal := rotateLetter(r, shiftKey, letterSize)
+		result.WriteRune(rotatedVal)
+
 	}
 	return result.String()
 }
